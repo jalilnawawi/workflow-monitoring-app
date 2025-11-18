ca42bbb91e8d142aa09e0d3280ade176a5092494
jalilnawawi <ajnawawi@gmail.com>
2025-11-18 21:58:46 +0700

    init repo

---
+++ src/main/java/jalilspringproject/workflow_monitoring_app/WorkflowMonitoringAppApplication.java
@@ -0,0 +1,13 @@
+package jalilspringproject.workflow_monitoring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowMonitoringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowMonitoringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jalilspringproject/workflow_monitoring_app/WorkflowMonitoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jalilspringproject.workflow_monitoring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowMonitoringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
