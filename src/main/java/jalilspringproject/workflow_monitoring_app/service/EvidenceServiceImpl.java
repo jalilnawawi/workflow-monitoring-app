af31ae12476a6a342ee4fbd0f3d902f399021ee0
jalilnawawi <ajnawawi@gmail.com>
2025-11-19 10:17:36 +0700

    [BUILD] service interface for Evidence

--- src/main/java/jalilspringproject/workflow_monitoring_app/service/EvidenceServiceImpl.java
+++ src/main/java/jalilspringproject/workflow_monitoring_app/service/EvidenceServiceImpl.java
@@ -2,6 +2,7 @@ package jalilspringproject.workflow_monitoring_app.service;
 
 import jalilspringproject.workflow_monitoring_app.model.dto.evidence.request.EvidenceRequestDto;
 import jalilspringproject.workflow_monitoring_app.model.dto.evidence.response.EvidenceResponseDto;
+import jalilspringproject.workflow_monitoring_app.model.dto.evidence.response.GetEvidenceResponseDto;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +10,25 @@ public class EvidenceServiceImpl implements EvidenceService {
     @Override
     public EvidenceResponseDto createEvidence(EvidenceRequestDto evidenceRequestDto) {
         return null;
+    }
+
+    @Override
+    public GetEvidenceResponseDto getAllEvidence() {
+        return null;
+    }
+
+    @Override
+    public GetEvidenceResponseDto getEvidenceById(Long id) {
+        return null;
+    }
+
+    @Override
+    public EvidenceResponseDto updateEvidence(Long id, EvidenceRequestDto evidenceRequestDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteEvidence(Long id) {
+
     }
 }
