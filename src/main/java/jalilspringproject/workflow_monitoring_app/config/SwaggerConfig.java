cdc8d71345f4e830e51d90ce879bec15cf547cd6
jalilnawawi <ajnawawi@gmail.com>
2025-11-19 09:41:50 +0700

    [BUILD] re-setup swagger config

--- src/main/java/jalilspringproject/workflow_monitoring_app/config/SwaggerConfig.java
+++ src/main/java/jalilspringproject/workflow_monitoring_app/config/SwaggerConfig.java
@@ -1,10 +1,10 @@
 package jalilspringproject.workflow_monitoring_app.config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
